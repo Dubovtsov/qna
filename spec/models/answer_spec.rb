ca06f83cd2c42860d9620353d1cca853bb8d0bd0
Dubovtsov <dubwin@yandex.ru>
2020-01-31 15:57:13 +0300

    attached files answer

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Answer, type: :model do
 
   it { should validate_presence_of :body }
   it { should validate_length_of(:body).is_at_least(4) }
+
+  it 'have many attached file' do
+    expect(Answer.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
+  end
 end
