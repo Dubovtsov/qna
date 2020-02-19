e0d530b7462b2d8eccff134b3e78aff27986ed23
Dubovtsov <dubwin@yandex.ru>
2020-02-19 13:34:55 +0300

    cocoon work

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -20,16 +20,15 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require cocoon
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require cocoon
 //= require_tree .
 //
-
 
 $(document).on("turbolinks:load", function(){
   setTimeout(function () {
