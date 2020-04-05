512ef4ee6dbfcfa8b95a9c19c836c7a223e63bc3
ivan <ivan@ivans-Mac.local>
2020-04-05 20:26:10 +0300

    vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -6,20 +6,22 @@ class Vote < ApplicationRecord
   validates :value, inclusion: { in: [-1, 1], message: "You can't vote twice" }
 
   def self.vote_up(user, votable)
-    vote = Vote.where(user: user, votable: votable).find_or_initialize_by(user: user, votable: votable)
+    vote = Vote.where(user: user, votable: votable).find_or_create_by(user: user, votable: votable)
+    vote.value = 0 if vote.value.nil?
+    
     vote.value += 1
     destroy_if_revote(vote)
   end
 
   def self.vote_down(user, votable)
-    vote = Vote.where(user: user, votable: votable).find_or_initialize_by(user: user, votable: votable)
+    vote = Vote.where(user: user, votable: votable).find_or_create_by(user: user, votable: votable)
+    vote.value = 0 if vote.value.nil?
     vote.value -= 1
     destroy_if_revote(vote)
   end
 
   def self.destroy_if_revote(vote)
     return vote unless vote.value.zero?
-
     vote.destroy
     nil
   end
